a783d214da80531b02f68ae19b4664c09a2875f9
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-03-16 14:31:41 +0800

    test

--- ecf/test.c
+++ ecf/test.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /* 
- * test - tests the mysystem-ans program
+ * test.c - tests the mysystem-ans program
  *   if called with no args, intentionally segfault
  *   otherwise exit status is argv[1]
  */
 int main(int argc, char **argv) {
 
-    printf("hello from test (%d)\n", atoi(argv[1]));
+    printf("hello from test.c (%d)\n", atoi(argv[1]));
     if (argc == 1)
 	*(char *)main = 0; /* seg fault */
     else 
